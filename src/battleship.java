f520b6c820aef0ec5fcda4908b48844e633d20d6
Deepak Nadig Anantha <admin@deepaknadig.com>
2016-06-01 12:08:48 -0500

    Updated main class

--- src/battleship.java
+++ src/battleship.java
@@ -4,11 +4,13 @@
 public class battleship {
 
     private int[] locations;
+    private int numOfHits = 0;
 
     public void setLocations(int[] locations) {
         this.locations = locations;
     }
 
-    public String checkLocation(String userGuess) {
+    public String checkLocation(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
     }
 }
