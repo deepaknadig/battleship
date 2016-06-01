c07552047d41bb7a0244bc8debc90d99d016db1f
Deepak Nadig Anantha <admin@deepaknadig.com>
2016-06-01 12:03:36 -0500

    Added skeleton methods

--- src/battleship.java
+++ src/battleship.java
@@ -2,4 +2,13 @@
  * Created by dna on 6/1/16.
  */
 public class battleship {
+
+    private int[] locations;
+
+    public void setLocations(int[] locations) {
+        this.locations = locations;
+    }
+
+    public String checkLocation(String userGuess) {
+    }
 }
--- src/battleshipTest.java
+++ src/battleshipTest.java
@@ -3,6 +3,12 @@
  */
 public class battleshipTest {
     public static void main(String[] args) {
+        battleship b = new battleship();
+        int[] locations = {3,4,5};
+        b.setLocations(locations);
+
+        String userGuess = "3";
+        String result = b.checkLocation(userGuess);
 
     }
 }
