9fc656df5c5fcf7c7edd5e83dbc7081cf54635c0
Deepak Nadig Anantha <admin@deepaknadig.com>
2016-06-07 22:57:27 -0500

    v.1.03 - GameHelper updated

--- src/GameHelper.java
+++ src/GameHelper.java
@@ -1,8 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by dna on 6/2/16.
  */
 public class GameHelper {
     public String getInput(String prompt) {
+        String input = null;
+        System.out.println(prompt + "  ");
 
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+            input = reader.readLine();
+            if (input.length() == 0) {
+                return null;
+            }
+        }
+        catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+
+        return input;
     }
 }
